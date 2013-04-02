ca743f2f541aeef14342527081872a26bfc269ab
chengkai <chengkai.me@gmail.com>
2013-04-02 18:18:03 +0800

    import samplepp first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = '0dd60fe776d38b983193bd7ffa35ba0dcb80e66c56b3fa8fff08dece967c923283e75ff31c935a5773b97af53148c2240437e71295a036b5ba702188a85c8700'
